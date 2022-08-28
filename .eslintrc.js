007c88af719609b5ce6fe90fccec910113844f13
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-28 10:00:39 +0300

    updated all

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['some-other-config-you-use', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+import { square, diag } from './js/my-module.js';
+
+console.log(square(11)); // 121
+console.log(diag(4, 3)); // 5
